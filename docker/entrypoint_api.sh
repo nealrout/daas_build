6324640ebba07343f45d84ba853efcb132f01da4
nealrout <nroutson@gmail.com>
2025-02-15 08:47:21 -0500

    fixes for ports.

--- docker/entrypoint_api.sh
+++ docker/entrypoint_api.sh
@@ -5,13 +5,11 @@ set -e  # Exit immediately if a command fails
 WORKDIR=$(pwd)
 
 # Ensure PORT is set, or use default 8002
-PORT=${PORT:-8002}
+PORT=${PORT:-8000}
 
 # Replace the port in the supervisord config
 echo "Updating Gunicorn port in supervisord_api.conf to: $PORT"
-sed -i "s/0.0.0.0:8001/0.0.0.0:$PORT/g" /etc/supervisord.conf
-
-PORT=${PORT:-8002}
+sed -i "s/0.0.0.0:8000/0.0.0.0:$PORT/g" /etc/supervisord.conf
 
 # Run collectstatic
 echo "Collecting static files..."
--- docker/entrypoint_auth.sh
+++ docker/entrypoint_auth.sh
@@ -5,13 +5,11 @@ set -e  # Exit immediately if a command fails
 WORKDIR=$(pwd)
 
 # Ensure PORT is set, or use default 8001
-PORT=${PORT:-8001}
+PORT=${PORT:-8000}
 
 # Replace the port in the supervisord config
 echo "Updating Gunicorn port in supervisord_api.conf to: $PORT"
-sed -i "s/0.0.0.0:8001/0.0.0.0:$PORT/g" /etc/supervisord.conf
-
-PORT=${PORT:-8001}
+sed -i "s/0.0.0.0:8000/0.0.0.0:$PORT/g" /etc/supervisord.conf
 
 # Run collectstatic
 echo "Collecting static files..."
