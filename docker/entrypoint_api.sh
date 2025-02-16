06fad5121a3769f1355736c8e605bff7aa66dd73
nealrout <nroutson@gmail.com>
2025-02-16 09:59:27 -0500

    port changes

--- docker/entrypoint_api.sh
+++ docker/entrypoint_api.sh
@@ -4,12 +4,12 @@ set -e  # Exit immediately if a command fails
 # Get the current working directory (WORKDIR from Dockerfile)
 WORKDIR=$(pwd)
 
-# Ensure PORT is set, or use default 8002
-PORT=${PORT:-8000}
+# Ensure PORT is set, or use default 9000
+PORT=${PORT:-9000}
 
 # Replace the port in the supervisord config
 echo "Updating Gunicorn port in supervisord_api.conf to: $PORT"
-sed -i "s/0.0.0.0:8000/0.0.0.0:$PORT/g" /etc/supervisord.conf
+sed -i "s/0.0.0.0:9000/0.0.0.0:$PORT/g" /etc/supervisord.conf
 
 # Run collectstatic
 echo "Collecting static files..."
